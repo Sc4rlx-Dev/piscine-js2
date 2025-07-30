c06113aae12689fd3eb399742b447680213556ed
oerraoui <itsscarl2022@gmail.com>
2025-07-30 12:27:27 +0100

    mutability : make shallow copy to person object , freeze , reference copy editing

---
+++ mutability.js
@@ -0,0 +1,19 @@
+// const person = {
+//   name: 'Rick',
+//   age: 77,
+//   country: 'US',
+// }
+
+const clone1 = {...person}
+const clone2 = {...person}
+Object.freeze(clone1)
+Object.freeze(clone2)
+
+// console.log(clone1)
+// console.log(clone2)
+
+const samePerson = person
+samePerson.age = 1 ;
+samePerson.country = 'FR';
+
+// console.log(person)
