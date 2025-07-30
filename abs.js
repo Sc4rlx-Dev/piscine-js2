df2078252c0aa5a3897214fb8d3b47689b8d9407
oerraoui <itsscarl2022@gmail.com>
2025-07-30 16:44:27 +0100

    abs : functions | positive number checker , absulute value | fix zero value

--- abs.js
+++ abs.js
@@ -1,5 +1,4 @@
-function isPositive(n){ return n >= 0 ? true : false }
-function abs(n){ return isPositive(n) == false ? n * -1 : n}
-
+function isPositive(n){ return n > 0 ? true : false }
+function abs(n){ return n == 0 ? 0 : (n > 0 ? n : -n)}
 
 // console.log(abs(0))
