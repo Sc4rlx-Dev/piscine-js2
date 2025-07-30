d3b904ee41847952e645147f1cf7b10eb9a44cbc
oerraoui <itsscarl2022@gmail.com>
2025-07-30 16:33:05 +0100

    method-man : string manipulation using methods

---
+++ method-man.js
@@ -0,0 +1,13 @@
+function words(str) { return str.split(" ") }
+function sentence(arr) { return arr.join(" ") }
+function yell(str) { return str.toUpperCase() }
+function whisper(str) { return `*${str.toLowerCase()}*`}
+function capitalize(str) { return str[0].toUpperCase() + str.slice(1).toLowerCase() }
+
+// console.log(words("test is fuj"));
+// console.log(sentence(["this" , "is" , "funy"]))
+// console.log(yell("test"))
+// console.log(capitalize("TEST"))
+
+
+
