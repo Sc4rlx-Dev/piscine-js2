0c3b97b4e21b33d485a3a838bde21dd6936bc0a8
oerraoui <itsscarl2022@gmail.com>
2025-08-01 15:54:41 +0100

    Keep-cut : manipulation functions for cutting and keeping characters using slice | handle len <= 3  characters case

--- keep-cut.js
+++ keep-cut.js
@@ -3,5 +3,8 @@ function cutLast(str) { return str.slice(0 , -2)}
 function cutFirstLast(str) {return cutLast(cutFirst(str))}
 function keepFirst(str) {return str.slice(0 , 2)}
 function keepLast(str) {return str.slice(-2)}
-function keepFirstLast(str) {return str.length > 2 ? keepFirst(str) + keepLast(str) : str}
+function keepFirstLast(str) {return str.length > 3 ? keepFirst(str) + keepLast(str) : str}
 
+
+console.log(keepFirstLast('afd'))
+console.log(keepFirstLast('afasdaad'))
