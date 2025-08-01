d7855339f71e8c5de1f45be9f68a24289c64c198
oerraoui <itsscarl2022@gmail.com>
2025-08-01 15:53:33 +0100

    Keep-cut : manipulation functions for cutting and keeping characters using slice | handle len > 2 characters case

--- keep-cut.js
+++ keep-cut.js
@@ -3,4 +3,5 @@ function cutLast(str) { return str.slice(0 , -2)}
 function cutFirstLast(str) {return cutLast(cutFirst(str))}
 function keepFirst(str) {return str.slice(0 , 2)}
 function keepLast(str) {return str.slice(-2)}
-function keepFirstLast(str) {return keepFirst(str) + keepLast(str)}
+function keepFirstLast(str) {return str.length > 2 ? keepFirst(str) + keepLast(str) : str}
+
