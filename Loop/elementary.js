7a6602d247ec08d38b7f010d697c50f3defb3e69
oerraoui <itsscarl2022@gmail.com>
2025-08-01 10:58:23 +0100

    elementary : arithmetic functions | implement multiply, divide, and modulo | fix (*) operator in devide()

---
+++ Loop/elementary.js
@@ -0,0 +1,36 @@
+function multiply(a , b) {
+    let res = 0
+    let flag_a = false, flag_b = false
+
+    if( a < 0) {flag_a = true ; a = -a} 
+    if( b < 0) {flag_b = true ; b = -b}
+
+    for(let i = 1 ; i <= b ; i++ ) {
+        res += a
+    }
+    if(flag_a != flag_b){ return -res }
+return res
+}
+
+function divide(a, b) {
+    let sign = 1
+    if(a < 0){ a = multiply(a, -1); sign = multiply(sign, -1) }
+    if(b < 0){ b = multiply(b, -1); sign = multiply(sign, -1) }
+
+    let p = 0
+    let q = 0
+
+    while(p + b <= a){
+        q++
+        p = multiply(q , b)
+    }
+
+return multiply(q , sign)
+}
+
+function modulo(a , b) {
+    let q = divide(a, b)
+    let p = multiply(q, b)
+    return a - p
+}
+
