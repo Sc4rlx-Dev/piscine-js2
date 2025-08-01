a36a681643136fcf11ccb641bf0efce61a9d39dc
oerraoui <itsscarl2022@gmail.com>
2025-08-01 17:16:08 +0100

    cut-corners : implement functions trunc floor , ceil , round

--- cut-corners.js
+++ cut-corners.js
@@ -1,43 +1,60 @@
+
 function trunc(n) {
-    sign = 1
-    let count = 0
-    if(n < 0){
-        sign = - 1
-        n *= -1
-    }
-    for(i = 0 ; i < n - 1 ; i++){
-        count ++
-    }
-return count * sign
+    let str = n + ''
+    let res = ''
+    
+    for (let i = 0 ; i < str.length ; i++) {
+        let c = str[i]
+        if (c === '.') break;
+        res += c
+    } 
+return +res
 }
-function round() {}
-function ceil() {}
-function floor() {}
+
+function floor(n) { return n < 0 && n !== trunc(n) ? trunc(n) - 1 : trunc(n)}
+
+function ceil(n) { return n > 0 && n !== trunc(n) ? trunc(n) + 1 : trunc(n) }
+
+function round(n) { return floor( n + 0.5) }
 
 
-
-// console.log(trunc(-0.123))
 
 // Using Math.trunc
 // console.log(Math.trunc(13.6));
+// console.log(trunc(13.6));
 // console.log(Math.trunc(-13.6));
+// console.log(trunc(-13.6));
 // console.log(Math.trunc(0.123));
+// console.log(trunc(0.123));
 // console.log(Math.trunc(-0.123));
+// console.log(trunc(-0.123));
 
 // // Using Math.round
 // console.log(Math.round(13.6));
+// console.log(round(13.6));
 // console.log(Math.round(13.4));
+// console.log(round(13.4));
 // console.log(Math.round(-13.6));
+// console.log(round(-13.6));
 // console.log(Math.round(-13.4));
+// console.log(round(-13.4));
 
 // // Using Math.floor
 // console.log(Math.floor(13.4));
+// console.log(floor(13.4));
 // console.log(Math.floor(13.6));
+// console.log(floor(13.6));
 // console.log(Math.floor(-13.4));
+// console.log(floor(-13.4));
 // console.log(Math.floor(-13.6));
+// console.log(floor(-13.6));
 
 // // Using Math.ceil
 // console.log(Math.ceil(13.4));
+// console.log(ceil(13.4));
 // console.log(Math.ceil(13.6));
+// console.log(ceil(13.6));
 // console.log(Math.ceil(-13.4));
+// console.log(ceil(-13.4));
 // console.log(Math.ceil(-13.6));
+// console.log(ceil(-13.6));
