da61f4355d674d2512bcfe760901c8c8304358df
oerraoui <itsscarl2022@gmail.com>
2025-08-01 18:44:00 +0100

    cut-corners : implement functions trunc floor , ceil , round | use elementary

--- cut-corners.js
+++ cut-corners.js
@@ -1,14 +1,7 @@
 
 function trunc(n) {
-    let str = n + ''
-    let res = ''
-    
-    for (let i = 0 ; i < str.length ; i++) {
-        let c = str[i]
-        if (c === '.') break;
-        res += c
-    } 
-return +res
+    const res = n - modulo(n , 1)
+    return res === 0 && n < 0 ? -0 : res
 }
 
 function floor(n) { return n < 0 && n !== trunc(n) ? trunc(n) - 1 : trunc(n)}
@@ -58,3 +51,40 @@ function round(n) { return floor( n + 0.5) }
 // console.log(ceil(-13.4));
 // console.log(Math.ceil(-13.6));
 // console.log(ceil(-13.6));
+
+
+function multiply(a , b) {
+    let res = 0
+    let flag_a = false, flag_b = false
+
+    if( a < 0) {flag_a = true ; a = -a} 
+    if( b < 0) {flag_b = true ; b = -b}
+
+    for(let i = 1 ; i <= b ; i++ ) {
+        res += a
+    }
+    if(flag_a != flag_b){ return -res }
+return res
+}
+
+function divide(a, b) {
+    let sign = 1
+    if(a < 0){ a = multiply(a, -1); sign = multiply(sign, -1) }
+    if(b < 0){ b = multiply(b, -1); sign = multiply(sign, -1) }
+
+    let p = 0
+    let q = 0
+
+    while(p + b <= a){
+        q++
+        p = multiply(q , b)
+    }
+
+return multiply(q , sign)
+}
+
+function modulo(a , b) {
+    let q = divide(a, b)
+    let p = multiply(q, b)
+    return a - p
+}
