a1769b1a2191489bec1db6c36a2ede038dd72ffb
oerraoui <itsscarl2022@gmail.com>
2025-08-02 12:08:44 +0100

    unbreakable : add split and join functions for string manipulation |

---
+++ Loop/unbreakable.js
@@ -0,0 +1,28 @@
+function split(str, sep) {
+    if (sep === "") return [...str]
+    if (str === "") return [""]
+    let arr = []
+    let i = 0
+    while (i < str.length) {
+        let index = str.indexOf(sep, i)
+        if (index === -1) {
+            arr.push(str.slice(i))
+            break
+        }
+        arr.push(str.slice(i, index))
+        i = index + sep.length
+    }
+    if (str.slice(-sep.length) === sep) arr.push("")
+    return arr
+}
+
+function join(arr , sep) {
+    let res = ''
+    if(!sep){sep = ','}
+
+    for(let i = 0 ; i < arr.length; i++){
+        res += arr[i]
+        if(i < arr.length - 1){ res += sep}
+    }
+return res
+}
