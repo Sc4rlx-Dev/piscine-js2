262d40cd0c7f0883fe6093f2e5e57437e4c355b3
oerraoui <itsscarl2022@gmail.com>
2025-08-02 16:41:28 +0100

    pyramid : pyramid function to generate string pyramid

---
+++ Loop/pyramid.js
@@ -0,0 +1,13 @@
+function pyramid(str, n) {
+    let res = ''
+    for (let i = 1; i <= n; i++) {
+        const s = " ".repeat((n - i) * str.length) 
+        const ch = str.repeat(2 * i - 1)
+        res += s + ch + (i < n ? '\n' : '')
+    }
+return res
+}
+
+
+
+
