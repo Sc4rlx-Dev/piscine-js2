0b49612125e12bbda3fedc12d879949badf92514
oerraoui <itsscarl2022@gmail.com>
2025-08-03 10:10:57 +0100

    delete duplicated files

--- flat.js
+++
@@ -1,8 +0,0 @@
-function flat(arr , n = 1){
-    const f = []
-    for(const elem of arr){
-        if(Array.isArray(elem) && n > 0 ){ f.push(...flat(elem, n - 1))}
-        else { f.push(elem) }
-    }
-return f
-}
