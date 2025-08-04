eae4e931c5e374b3c7c990072f1df92376088cc9
oerraoui <itsscarl2022@gmail.com>
2025-08-04 15:48:13 +0100

    group-price : extract numeric values from price strings | fix metching coins

--- group-price.js
+++ group-price.js
@@ -1,12 +1,16 @@
-var r = /\d+/g
+function groupPrice(price) {
+    const g = /([a-zA-Z$]+)(\d+)\.(\d+)/g
+    let res = []
+    let match
 
-function groupPrice(price){
-    let full = [[price]]
-    
-    for(let match = r.exec(price) ; match != null ; match = r.exec(price)){
-        full[0].push(match[0])
-    }
-return full
+    while ((match = g.exec(price)) !== null) {
+        const m = match[0] 
+        const dlr = match[2]    
+        const cnt = match[3]     
+        
+        res.push([m, dlr, cnt])
+    }    
+return res
 }
-
-// console.log(groupPrice("USD12.13"))
+// console.log(groupPrice('The price of the cereals is $4.00.'), [['$4.00', '4', '00']])
+// console.log(groupPrice('the total is USD19.98'), [['USD19.98', '19', '98']])
