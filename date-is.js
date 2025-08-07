3e470a9a35ddd36f06475af46accdc41d17aaddc
oerraoui <itsscarl2022@gmail.com>
2025-08-07 22:20:02 +0100

    date-is : date validation functions: isValid, isAfter, isBefore, isFuture, isPast | fix dates

--- date-is.js
+++ date-is.js
@@ -1,20 +1,31 @@
-function isValid(date) { return Object.prototype.toString.call(date) === '[object Date]' && !isNaN(date)}
+function isValid(date) {
+    if (typeof date === 'number') return !isNaN(new Date(date).getTime())
+    if (date instanceof Date) return !isNaN(date.getTime())
+    return false
+}
 
-function isAfter(d1 , d2) { return Date.parse(d1) > Date.parse(d2) ? true : false}
+function isAfter(d1, d2) {
+    const date1 = (d1 instanceof Date ? d1 : new Date(d1))
+    const date2 = (d1 instanceof Date ? d2 : new Date(d2))
+    return date1.getTime() > date2.getTime()
+}
 
-function isBefore(d1 , d2) { return !isAfter(d1 , d2)}
+function isBefore(d1, d2) {
+    const date1 = (d1 instanceof Date ? d1 : new Date(d1))
+    const date2 = (d1 instanceof Date ? d2 : new Date(d2))
+    return date1.getTime() < date2.getTime()
+}
 
 function isFuture(date) { return isValid(date) && isAfter(date, new Date()) }
 
-function isPast(date) { return !isFuture(date) }
+function isPast(date) { return isValid(date) && isBefore(date, new Date()) }
 
 
-
-// console.log(new Date())
+// const date = new Date()
 
 
 // const date = new Date(1488370835081); // Replace NaN with a valid timestamp
 // console.log(isValid(date))
 // console.log(isAfter(new Date(1989, 6, 10), new Date(1987, 1, 11)))
-// console.log(isBefore(new Date(1989, 6, 10), new Date(1987, 1, 11)))
+// console.log(isBefore(new Date(1987, 1, 11), new Date(1989, 6, 10)))
  
