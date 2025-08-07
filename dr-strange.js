5d10573bdff57939516607947b809a094aac611a
oerraoui <itsscarl2022@gmail.com>
2025-08-07 21:22:13 +0100

    dr-strange : add week and time travel functions with example usage

---
+++ dr-strange.js
@@ -0,0 +1,24 @@
+var week = [    'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday',
+                'secondMonday', 'secondTuesday', 'secondWednesday', 'secondThursday', 'secondFriday', 'secondSaturday', 'secondSunday'
+]
+
+function addWeek(date) {
+    const d = new Date('0001-01-01')
+    const dif = Math.floor((date.getTime() - d.getTime()) / (1000 * 60 * 60 * 24))
+    const ind = dif % 14
+    return week[ind]
+}
+
+function timeTravel({ date, h = 0, m = 0, s = 0 }) {
+    const Date = new Date(date)
+    Date.setHours(h) ; Date.setMinutes(m) ; Date.setSeconds(s)
+    return Date
+}
+
+console.log(addWeek(new Date('0001-01-01')))
+console.log(addWeek(new Date('0001-01-02')))
+console.log(addWeek(new Date('0001-01-07')))
+console.log(addWeek(new Date('0001-01-08')))
+console.log(addWeek(new Date('0001-01-09')))
+
+console.log(timeTravel({ date: new Date('2020-05-29 23:25:22'), hour: 21, minute: 22, second: 10 }))
