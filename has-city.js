c071275b19b4041c4577f9f601739d968cf2d5ab
Oussama Erraoui <oerraoui>
2025-08-10 08:15:26 +0100

    has-city : manipulate string format

--- has-city.js
+++ has-city.js
@@ -1,1 +1,1 @@
-const hasCity = (cntry , cityes) => city => `${city} is ${cityes.include(city) ? '' : 'not'} a city from ${cntry}`
+const hasCity = (cntry, cityes) => city => `${city} is ${cityes.includes(city) ? '' : 'not '}a city from ${cntry}`;
