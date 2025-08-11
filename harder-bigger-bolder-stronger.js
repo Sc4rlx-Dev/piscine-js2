3d90900c81a9dbc5a7b8e20f4e08ad241ba0a0f9
oerroaui <itsscarl2022@gmail.com>
2025-08-11 22:51:54 +0100

    harder-bigger-bolder-stronger : generateLetters function to create styled alphabet letters

---
+++ harder-bigger-bolder-stronger.js
@@ -0,0 +1,9 @@
+export const generateLetters = () => {
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    for (let i = 0, pixel = 10 , len = chars.length; i <= 119; i++ , pixel++){
+        const c = document.createElement('div') ; document.body.append(c)
+        c.textContent = chars[Math.floor(Math.random() * len)]
+        c.style.fontSize = `${pixel}px`
+        c.style.fontWeight = i < 40 ? 300 : i < 80 ? 400 : 600
+    }
+}
