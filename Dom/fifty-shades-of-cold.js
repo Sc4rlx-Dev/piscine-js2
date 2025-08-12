e122740b48949eecec6865e49702e774092937db
oerraoui <itsscarl2022@gmail.com>
2025-08-12 02:30:33 +0100

    fifty-shades-of-cold : generateClasses, generateColdShades, and choseShade functions for color management

---
+++ Dom/fifty-shades-of-cold.js
@@ -0,0 +1,30 @@
+import { colors } from '../fifty-shades-of-cold.data.js'
+
+export function generateClasses() {
+    const s = document.createElement('style')
+    let cs = ''
+    colors.forEach( c => { cs += `.${c}{background:${c};}`})
+    s.textContent = cs
+    document.head.appendChild(s)
+}
+
+export function generateColdShades() {
+    const key = ['aqua', 'blue', 'turquoise', 'green', 'cyan', 'navy' , 'purple']
+    const colr = colors.filter(c => key.find(k => c.includes(k)))
+    // console.log(colr)
+
+    colr.forEach(sh => {
+        const d= document.createElement('div')
+        d.className=sh
+        d.textContent=sh
+        d.addEventListener('click', () => choseShade(sh))
+        document.body.appendChild(d)})
+}   
+
+export function choseShade(sh) {
+    const all = document.querySelectorAll('div')
+    all.forEach(d => { d.className = sh })
+}
+
+
+
