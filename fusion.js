2d5095ab0f74e44c28f632a95e1095174b5ec3bd
oerraoui <itsscarl2022@gmail.com>
2025-08-15 22:25:56 +0100

    fusion :  merge two objects with various data types

--- fusion.js
+++ fusion.js
@@ -1,21 +1,22 @@
 export const fusion = (a, b) => {
-    const res = { ...a }
+    const res = {...a}
 
     for (const k in b) {
         const av = res[k]
         const bv = b[k]
-        console.log(av ,bv)
         res[k] = Array.isArray(av) && Array.isArray(bv) ? [...av, ...bv]
         : typeof av === 'object' && av !== null && !Array.isArray(av) && typeof bv === 'object' && bv !== null && !Array.isArray(bv) ? fusion(av, bv)
-        : typeof (av && bv) === 'number' ? av + bv
-        : typeof (av && bv) === 'string' ? `${av} ${bv}`
+        : typeof av === 'number' && typeof bv === 'number' ? av + bv
+        : typeof av === 'string' && typeof bv === 'string' ? `${av} ${bv}`
         : bv
     }
 return res
 }
 
+
+
 // console.log(fusion({ arr: [1, 2 , 3] }, { arr: [4] }))
-console.log(fusion({ a: { b: 1 } }, { a: 1 }).a, 1)
+// console.log(fusion({ a: { b: 1 } }, { a: 1 }).a, 1)
 
 // console.log(fusion({ arr: [1, "2"] }, { arr: [2] }))
 // -> { arr: [ 1, '2', 2 ] }
@@ -24,6 +25,8 @@ console.log(fusion({ a: { b: 1 } }, { a: 1 }).a, 1)
 // -> { str: 'salem alem' }
 
 // console.log(fusion({ a: 10, b: 8, c: 1 }, { a: 10, b: 2 }))
+// console.log(fusion({ a: { b: 1 } }, { a: 1 }).a, 1)
+
 // -> { a: 20, b: 10, c: 1 }
 
 // console.log(fusion({ a: 1, b: { c: "Salem" } }, { a: 10, x: [], b: { c: "alem" } }))// -> { a: 11, x: [], b: { c: 'Salem alem' } }
